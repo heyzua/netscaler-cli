444d23f8a978913afb5b248e357bedd0c6374f0d
Gabe McArthur <madeonamac@gmail.com>
2011-03-18 11:02:37 -0700

    Adding more output to the netscaler server query, so that the state of the attached services can be determined.

--- lib/netscaler/server/response.rb
+++ lib/netscaler/server/response.rb
@@ -17,16 +17,18 @@ module Netscaler::Server
     end
 
     def services
-      @services ||= if @info[:servicename]
-                      res = @info[:servicename][:item]
-                      if res.is_a?(Array)
-                        res
-                      else
-                        [res]
-                      end
-                    else
-                      []
-                    end
+      @parsed_services ||= []
+      return @parsed_services if !@parsed_services.empty? || @info[:servicename].nil?
+
+      if @info[:servicename][:item].is_a?(String)
+        @parsed_services << ServiceInfo.new(@info, nil)
+      else
+        @info[:servicename][:item].each_with_index do |name, i|
+          @parsed_services << ServiceInfo.new(@info, i)
+        end
+      end
+
+      @parsed_services
     end
 
     def to_hash
@@ -37,10 +39,76 @@ module Netscaler::Server
       }
 
       if !services.empty?
-        hash[:services] = services
+        hash[:services] = services.map {|s| s.to_hash}
       end
 
       hash
+    end
+  end
+
+  class ServiceInfo
+    attr_reader :name, :type, :state, :ip_address, :servicegroup
+
+    def initialize(raw_response, index)
+      @name = raw_response[:servicename][:item]
+      @ip_address = raw_response[:serviceipaddress][:item]
+      @state = raw_response[:svrstate][:item]
+      @port = raw_response[:port][:item]
+      @type = raw_response[:servicetype][:item]
+
+      if !index.nil?
+        @name = @name[index]
+        @ip_address = @ip_address[index]
+        @state = @state[index]
+        @port = @port[index]
+        @type = @type[index]
+      end
+
+      @servicegroup = if raw_response[:servicegroupname] && raw_response[:servicegroupname][:item]
+                        ServiceGroupInfo.new(raw_response, index)
+                      else
+                        nil
+                      end
+    end
+
+    def port
+      @port.to_i
+    end
+
+    def to_hash
+      hash = {
+        :name => name,
+        :ip_address => ip_address,
+        :type => type,
+        :state => state,
+        :port => port,
+      }
+
+      if servicegroup
+        hash[:servicegroup] = servicegroup.to_hash
+      end
+
+      hash
+    end
+  end
+
+  class ServiceGroupInfo
+    attr_reader :name, :state
+
+    def initialize(raw_response, index)
+      @name = raw_response[:servicegroupname][:item]
+      @state = raw_response[:boundservicegroupsvrstate][:item]
+
+      if !index.nil?
+        @servicegroup = @servicegroup[index]
+        @servicegroup_state = @servicegroup_state[index]
+      end
+    end
+
+    def to_hash
+      { :name => name,
+        :state => state
+      }
     end
   end
 end
